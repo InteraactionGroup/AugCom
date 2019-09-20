0e1dbaf50983002610edcbea197825e50f2f06f2
kuhlkrein <sebastien.riou@etu.univ-grenoble-alpes.fr>
2019-09-20 14:40:03 +0200

    Editing panel can create basic new button with custom label and color.
    The choose a file section is used only to test how to import a json external file.

--- src/app/boxes/boxes.component.ts
+++ src/app/boxes/boxes.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Bouton } from '../data/cell';
-import { Board } from '../data/mockOpenBoard';
 import { BarcontentService } from '../service/barcontent.service';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 import {UserBarServiceService} from "../service/user-bar-service.service";
 import {EditionServiceService} from "../service/edition-service.service";
+import {BoardServiceService} from "../service/board-service.service";
 
 @Component({
   selector: 'app-boxes',
@@ -14,15 +14,10 @@ import {EditionServiceService} from "../service/edition-service.service";
 
 export class BoxesComponent implements OnInit {
 
-  constructor(private barService: BarcontentService ,private userBarServiceService: UserBarServiceService,private editionServiceService: EditionServiceService) {
+  constructor(private boardServiceService : BoardServiceService,private barService: BarcontentService ,private userBarServiceService: UserBarServiceService,private editionServiceService: EditionServiceService) {
   }
-
-  board = Board;
-  boxes = this.board.boutons;
   selectedBox: Bouton = null;
   prevselectedBox: Bouton = null;
-  folder = 'root';
-   public backgroundImg: SafeStyle;
 
   ngOnInit() {
   }
@@ -41,18 +36,18 @@ export class BoxesComponent implements OnInit {
   onSelectFolder(box: Bouton): void {
    this.prevselectedBox = this.selectedBox;
    this.selectedBox = box;
-   this.folder = box.id;
+   this.boardServiceService.folder = box.id;
   }
 
   getImgUrl(box: Bouton): string {
-    const s = this.board.images.find(x => x.id === box.imageId).path;
+    const s = this.boardServiceService.board.images.find(x => x.id === box.imageId).path;
     return 'url(' + s + ')';
   }
 
 
 
   onSelectBack(): void {
-    this.folder = this.boxes.find(x => x.id === this.folder).extCboardLabelKey;
+    this.boardServiceService.folder = this.boardServiceService.board.boutons.find(x => x.id === this.boardServiceService.folder).extCboardLabelKey;
   }
 
   onSelectAdd(): void{
