7dca1436610ea213d5ad03a5198c69e035bb635a
herved <david.herve99@gmail.com>
2020-05-07 10:52:38 +0200

    work on chrome

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, Input, AfterViewInit, NgZone } from "@angular/core";
+import { Component, OnInit, Input, AfterViewInit } from "@angular/core";
 import { Element } from "../../types";
 import { UsertoolbarService } from "../../services/usertoolbar.service";
-import { IndexeddbaccessService } from "../../services/indexeddbaccess.service";
 import * as Muuri from "muuri";
 
 @Component({
@@ -12,31 +11,33 @@ import * as Muuri from "muuri";
 export class GridComponent implements OnInit, AfterViewInit {
   @Input() elements: Element[];
 
-  grid: any;
-
-  constructor(
-    public userToolbarService: UsertoolbarService,
-    private zone: NgZone
-  ) {
+  constructor(public userToolbarService: UsertoolbarService) {
     Muuri.defaultOptions.dragSortPredicate.action = "swap";
   }
 
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.grid = new Muuri(".grid", {
-        layoutEasing: "cubic-bezier(0.215, 0.61, 0.355, 1)",
-        dragEnabled: true,
-        dragStartPredicate: (item, e) => this.checkIfEditEnabled(item, e),
-      });
-    });
-  }
+  ngOnInit(): void {}
 
-  ngAfterViewInit(): void {}
+  ngAfterViewInit(): void {
+    this.doAsyncTask();
+  }
 
   checkIfEditEnabled(item, e) {
     console.log("check");
     return this.userToolbarService.edit
       ? Muuri.ItemDrag.defaultStartPredicate(item, e)
       : false;
+  }
+
+  async doAsyncTask(): Promise<any> {
+    return await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        new Muuri(".grid", {
+          layoutEasing: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+          dragEnabled: true,
+          dragStartPredicate: (item, e) => this.checkIfEditEnabled(item, e),
+        });
+      }, 1000);
+      resolve("grid init");
+    });
   }
 }
