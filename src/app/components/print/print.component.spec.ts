c58c88756ff0fe1c3cba8525fc8e48aa19d83136
kuhlkrein <sebastien.riou@univ-grenoble-alpes.fr>
2020-05-12 16:28:23 +0200

    fix test issue for print component ng2imgmax service not found

--- src/app/components/print/print.component.spec.ts
+++ src/app/components/print/print.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PrintComponent } from './print.component';
+import {Ng2ImgMaxModule} from "ng2-img-max";
 
 describe('PrintComponent', () => {
   let component: PrintComponent;
@@ -8,7 +9,8 @@ describe('PrintComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PrintComponent ]
+      declarations: [ PrintComponent ],
+      imports: [Ng2ImgMaxModule],
     })
     .compileComponents();
   }));
