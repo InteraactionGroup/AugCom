d25860be0115564c2fd6858f8b613a28b62865d0
PillouMatou <vincent.armone@etu.univ-grenoble-alpes.fr>
2021-07-26 15:55:11 +0200

    close the dialog when we add a user + fix tests

--- src/app/components/dialog-add-user/dialog-add-user.component.spec.ts
+++ src/app/components/dialog-add-user/dialog-add-user.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DialogAddUserComponent } from './dialog-add-user.component';
+import {Ng2ImgMaxModule} from "ng2-img-max";
+import {FormsModule} from "@angular/forms";
 
 describe('DialogAddUserComponent', () => {
   let component: DialogAddUserComponent;
@@ -8,7 +10,8 @@ describe('DialogAddUserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DialogAddUserComponent ]
+      declarations: [ DialogAddUserComponent ],
+      imports: [Ng2ImgMaxModule, FormsModule]
     })
     .compileComponents();
   }));
