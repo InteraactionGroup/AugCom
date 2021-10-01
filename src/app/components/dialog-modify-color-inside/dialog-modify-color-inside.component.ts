79e1f74ca7ff04a7661bc466e549ffb3de08c4c7
PillouMatou <vincent.armone@etu.univ-grenoble-alpes.fr>
2021-10-01 17:22:55 +0200

    add multilang

--- src/app/components/dialog-modify-color-inside/dialog-modify-color-inside.component.ts
+++ src/app/components/dialog-modify-color-inside/dialog-modify-color-inside.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {PaletteService} from "../../services/palette.service";
 import {EditionService} from "../../services/edition.service";
+import {MultilinguismService} from "../../services/multilinguism.service";
 
 @Component({
   selector: 'app-dialog-modify-color-inside',
@@ -10,7 +11,8 @@ import {EditionService} from "../../services/edition.service";
 export class DialogModifyColorInsideComponent implements OnInit {
 
   constructor(public paletteService: PaletteService,
-              public editionService: EditionService) { }
+              public editionService: EditionService,
+              public multilinguism: MultilinguismService) { }
 
   ngOnInit(): void {
   }
