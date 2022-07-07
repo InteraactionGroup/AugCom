525fa3373c4f2096662fcd23fbc068908fbcebb8
PillouMatou <vincent.arnone@hotmail.fr>
2022-07-07 14:10:42 +0200

    fix tests

--- src/app/components/share/share.component.spec.ts
+++ src/app/components/share/share.component.spec.ts
@@ -50,15 +50,17 @@ describe('ShareComponent', () => {
   //   expect(allListElements[5].textContent).toContain(component.multilinguism.translate('importProloquo'));
   // });
 
-  it('should create the 4 different options components', () => {
+  it('should create the 7 different options components', () => {
     const compiled = fixture.debugElement.nativeElement;
     const allListElements = compiled.querySelectorAll('.listElement');
-    expect(allListElements.length).toEqual(5/*6*/);
+    expect(allListElements.length).toEqual(7/*6*/);
     expect(allListElements[0].textContent).toContain(component.multilinguism.translate('importSave'));
     expect(allListElements[1].textContent).toContain(component.multilinguism.translate('import save from snap core first'));
     expect(allListElements[2].textContent).toContain(component.multilinguism.translate('import save from life companion'));
-    expect(allListElements[3].textContent).toContain(component.multilinguism.translate('exportSave'));
-    expect(allListElements[4].textContent).toContain(component.multilinguism.translate('exportPDF'));
+    expect(allListElements[3].textContent).toContain(component.multilinguism.translate('exportPage'));
+    expect(allListElements[4].textContent).toContain(component.multilinguism.translate('exportPageAndItsSubset'));
+    expect(allListElements[5].textContent).toContain(component.multilinguism.translate('exportSave'));
+    expect(allListElements[6].textContent).toContain(component.multilinguism.translate('exportPDF'));
   });
 
 });
