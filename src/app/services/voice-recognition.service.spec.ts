3702aafc3787f832c830563be5bbac433b6ad8a4
PillouMatou <vincent.arnone@hotmail.fr>
2022-09-09 17:21:38 +0200

    fix test

--- src/app/services/voice-recognition.service.spec.ts
+++ src/app/services/voice-recognition.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { VoiceRecognitionService } from './voice-recognition.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('VoiceRecognitionService', () => {
   let service: VoiceRecognitionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientModule]
+    });
     service = TestBed.inject(VoiceRecognitionService);
   });
 
