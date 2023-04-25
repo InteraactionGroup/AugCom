a86e26ca9d90c8b2255f082b78ce97d432b0e30d
NihachiOokami <paulsode05@gmail.com>
2023-04-25 15:56:36 +0200

    Autocompletion & autosearch on alternative image search
    Added autocompletion and autosearch for the alternative image's search field

--- src/app/components/alternative-forms/alternative-forms.component.ts
+++ src/app/components/alternative-forms/alternative-forms.component.ts
@@ -13,6 +13,10 @@ import {BoardService} from '../../services/board.service';
 import {MultilinguismService} from '../../services/multilinguism.service';
 import {ConfigurationService} from "../../services/configuration.service";
 
+import {Observable} from "rxjs";
+import {FormControl} from "@angular/forms";
+import {map, startWith} from "rxjs/operators";
+
 
 @Component({
   selector: 'app-alternative-forms',
@@ -41,7 +45,15 @@ export class AlternativeFormsComponent implements OnInit {
 
   wordList: string[] = [];
 
+  myControl = new FormControl();
+  filteredOptions: Observable<string[]>;
+
   ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges
+    .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+    );
   }
 
   saveCurrentElementForm() {
@@ -311,14 +323,14 @@ export class AlternativeFormsComponent implements OnInit {
 
     if(this.configurationService.LANGUAGE_VALUE === 'FR') {
       (arasaacJson as unknown as ArasaacObject)[0].wordList.forEach(word => {
-        if (text !== null && text !== '' && word.toLowerCase().includes(text.toLocaleLowerCase())) {
+        if (text !== null && text !== '' && word.toLowerCase().includes(text.toLocaleLowerCase()) && this.getSimilarity(text.toLowerCase(), word.toLowerCase()) >= 0.5) {
           const url = word;
           tempList.push({lib: 'arasaacNB', word: this.cleanString(url)});
         }
       }, this);
 
       (arasaacColoredJson as unknown as ArasaacObject)[0].wordList.forEach(word => {
-        if (text !== null && text !== '' && word.toLowerCase().includes(text.toLocaleLowerCase())) {
+        if (text !== null && text !== '' && word.toLowerCase().includes(text.toLocaleLowerCase()) && this.getSimilarity(text.toLowerCase(), word.toLowerCase()) >= 0.5) {
           const url = word;
           tempList.push({lib: 'arasaacColor', word: this.cleanString(url)});
         }
@@ -326,7 +338,7 @@ export class AlternativeFormsComponent implements OnInit {
     }
     else{
       (mullberryJson as unknown as MulBerryObject[]).forEach(value => {
-        if (text !== null && text !== '' && value.symbol.toLowerCase().includes(text.toLocaleLowerCase())) {
+        if (text !== null && text !== '' && value.symbol.toLowerCase().includes(text.toLocaleLowerCase()) && this.getSimilarity(text.toLowerCase(), value.symbol.toLowerCase()) >= 0.5) {
           const url = value.symbol;
           tempList.push({lib: 'mulberry', word: this.cleanString(url)});
         }
@@ -347,5 +359,62 @@ export class AlternativeFormsComponent implements OnInit {
 
   cleanString(t: string) {
     return t.replace(/'/g, '\\\'');
+  }
+
+  /**
+   * Checks the Levenshtein's distance (the similarity) between the two strings in param using Levenshtein's algorithm.
+   * https://en.wikipedia.org/wiki/Levenshtein_distance
+   * @param word1 First word to compare
+   * @param word2 Second word to compare
+   * @returns A float between 0 and 1. 0 means the two words are completely different; 1 means they are the same.
+   */
+  getSimilarity(word1, word2): number {
+    let longer = word1;
+    let shorter = word2;
+    if (word1.length < word2.length) {
+      longer = word2;
+      shorter = word1;
+    }
+    
+    return ((longer.length - this.distance(longer, shorter)) / parseFloat(longer.length));
+  }
+
+  /**
+   * The main body of Levenshtein's algorithm
+   * This should only be called by the method getSimilarity to avoid errors.
+   * @param s1 first word
+   * @param s2 second word
+   * @returns the "cost" to get from the first word to the second AKA their distance
+   */
+  distance(s1, s2) {
+    var costs = new Array();
+    for (var i = 0; i <= s1.length; i++) {
+      var lastValue = i;
+      for (var j = 0; j <= s2.length; j++) {
+        if (i == 0)
+          costs[j] = j;
+        else {
+          if (j > 0) {
+            var newValue = costs[j - 1];
+            if (s1.charAt(i - 1) != s2.charAt(j - 1))
+              newValue = Math.min(Math.min(newValue, lastValue),
+                costs[j]) + 1;
+            costs[j - 1] = lastValue;
+            lastValue = newValue;
+          }
+        }
+      }
+      if (i > 0)
+        costs[s2.length] = lastValue;
+    }
+    return costs[s2.length];
+  }
+
+  private _filter(value: string): string[] {
+    if(value.length > 1){
+      this.wordList = [];
+      this.searchInLib(value);
+      return this.wordList;
+    }
   }
 }
