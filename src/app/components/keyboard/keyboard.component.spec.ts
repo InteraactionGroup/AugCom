e6ce6059ebc6c4619e1fc7c232d25aff92455323
NihachiOokami <paulsode05@gmail.com>
2023-05-17 09:39:38 +0200

    Added RouterTestingModule to keyboard tests
    Added RouterTestingModule to keyboard component tests

--- src/app/components/keyboard/keyboard.component.spec.ts
+++ src/app/components/keyboard/keyboard.component.spec.ts
@@ -6,6 +6,7 @@ import {FormsModule} from '@angular/forms';
 import {Ng2ImgMaxModule} from 'ng2-img-max';
 import {Router} from '@angular/router';
 import {HttpClientModule} from "@angular/common/http";
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('KeyboardComponent', () => {
   let component: KeyboardComponent;
@@ -14,7 +15,7 @@ describe('KeyboardComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      imports: [FormsModule, Ng2ImgMaxModule, HttpClientModule],
+      imports: [FormsModule, Ng2ImgMaxModule, HttpClientModule, RouterTestingModule],
       providers: [{
         provide: Router, useClass: class {
           navigate = jasmine.createSpy('navigate');
