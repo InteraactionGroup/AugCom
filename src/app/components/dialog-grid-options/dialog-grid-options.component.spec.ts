0efd98bedf0cac653b4c99de78a0a8443d26297a
NihachiOokami <paulsode05@gmail.com>
2023-06-27 18:53:21 +0200

    Corrected import errors

--- src/app/components/dialog-grid-options/dialog-grid-options.component.spec.ts
+++ src/app/components/dialog-grid-options/dialog-grid-options.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DialogGridOptionsComponent } from './dialog-grid-options.component';
+import { HttpClient } from '@angular/common/http';
+import { Ng2ImgMaxModule } from 'ng2-img-max';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('DialogGridOptionsComponent', () => {
   let component: DialogGridOptionsComponent;
@@ -8,6 +12,7 @@ describe('DialogGridOptionsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClient, Ng2ImgMaxModule, MatDialogModule, BrowserAnimationsModule],
       declarations: [ DialogGridOptionsComponent ]
     })
     .compileComponents();
